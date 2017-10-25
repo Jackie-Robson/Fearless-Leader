60a8be1626fb074dd6459cf1e4e41af9f65cdeba
Jackie Robson <jackie.anne.robson@gmail.com>
2017-10-25 16:39:24 +0100

    Initial commit

---
+++ home.php
@@ -0,0 +1,91 @@
+<?php $container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<header>
+			<div class="hero-image">
+				<div class="hero-text">
+					<h1>Iain Anderson</h1>
+					<hr style="border:1px solid white; width:200px">
+					<h3>Fearless leader of Cicero group</h3>
+				</div>
+			</div>
+			<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+
+			<?php if ( 'container' == $container ) : ?>
+				<div class="container">
+			<?php endif; ?>
+
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<!-- The WordPress Menu goes here -->
+					<?php wp_nav_menu(
+						array(
+							'theme_location'  => 'primary',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'navbarNavDropdown',
+							'menu_class'      => 'navbar-nav',
+							'fallback_cb'     => '',
+							'menu_id'         => 'main-menu',
+							'walker'          => new WP_Bootstrap_Navwalker(),
+						)
+					); ?>
+				<?php if ( 'container' == $container ) : ?>
+				</div><!-- .container -->
+				<?php endif; ?>
+
+			</nav><!-- .site-navigation -->
+		</header><!-- Header -->
+		<section style="background-color: #545353; color: white;">
+			<div class="col-8" style="margin: 0px auto; padding: 1em;">
+				<article >
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+				</article>
+				<article style="margin: 1em auto" >
+					Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+				</article>
+
+			</div>
+		</section>
+		<div id="imageBreakleft">
+			<div class="hero-text">
+				<h1>Iain Anderson</h1>
+				<hr style="border:1px solid white; width:200px">
+				<h3>Fearless leader of Cicero group</h3>
+			</div>
+		</div>
+
+		<div id="imageBreakright">
+			<div class="hero-text">
+				<h1>Iain Anderson</h1>
+				<hr style="border:1px solid white; width:200px">
+				<h3>Fearless leader of Cicero group</h3>
+			</div>
+		</div>
+		<section style="background-color: black; color: white;">
+			<div class="col-8" style="margin: 0px auto; padding: 1em;">
+				<article >
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+				</article>
+				<article style="margin: 1em auto" >
+					Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+				</article>
+
+			</div>
+		</section>
+	</body>
+</html>
